5292d3e0974fa469a1d7b01ddb352aea565d0348
hemischbrilop <hemischbrilop@yahoo.com.mx>
2023-05-29 03:28:44 -0600

    Se realizo la verificacion de la contrasena

--- Iniciarsesion.php
+++ Iniciarsesion.php
@@ -11,4 +11,29 @@ $Contrasena = $_POST["Contrasena"];
 
 $Consulta="SELECT * FROM 'USUARIO' WHERE Correo_Electronico = '$Correo_Electronico' ";
 $Consulta= mysqli_query
+
+//Evaluacion de correo
+$Consulta = "SELECT * FROM 'Usuario' WHERE Correo_Electronico= '$Correo_Electronico' ";
+$Consulta = mysqli_query($CONEXION, $Consulta); //Se ejecuta la consulta
+$Consulta = mysqli_fetch_array($Consulta);
+
+// Apartir de aqui se comparara la contrasena
+if($Consulta)
+    if(password_verify($Contrasena, $Consulta['Contrasena'])){
+        $_SESSION[login] = true; 
+
+        $_SESSION[Nombre] = $Consulta['Nombre']
+        $_SESSION[Apellido] = $Consulta['Apellido']
+        $_SESSION[Edad] = $Consulta['Edad']
+        $_SESSION[Telefono] = $Consulta['Telefono']
+        $_SESSION[Correo_Electronico] = $Consulta['Correo_Electronico']
+        $_SESSION[Direccion] = $Consulta['Direccion']
+        $_SESSION[Ciudad] = $Consulta['Ciudad']
+        
+        header('Location: Ruta para pagina de perfil') //El hedader es el que redirecciona ES PHP NO HTML - QUE NO SE ME OLVIDE
+
+    }else{
+        echo"Contrasena incorrecta";
+        echo"<br><a href=''>Intentele otra vez.</a></div>:";
+    }
 ?>
