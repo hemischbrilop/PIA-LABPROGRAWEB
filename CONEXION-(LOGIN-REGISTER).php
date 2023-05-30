91ddb3b04f8821f3f54a2d5efaa6bfc324de2790
hemischbrilop <hemischbrilop@yahoo.com.mx>
2023-05-30 12:40:33 -0600

    Se agrego la verificacion del correo electronico

--- CONEXION-(LOGIN-REGISTER).php
+++ CONEXION-(LOGIN-REGISTER).php
@@ -17,7 +17,14 @@ $Correo_Electronico  = $_POST[Correo_Electronico];
 $ContrasenaHash = password_hash($Contrasena, PASSWORD_BCRYPT); // <--BCrypt es el algoritmo de encriptacion que es el que no devolvera una cadena de 60 digitos  
 $FotoPerfil = "link de la imagen"; // <-- aqui se almacena la imagen del foto de perfil
 
+//Aqui creamos la consulta evaluaremos si el correo ya se ha registrado
+$ConsultaCorreo_Electronico="SELECT Correo_Electronico FROM Usuario WHERE Correo_Electronico='$Correo_Electronico' ";
+$ConsultaCorreo_Electronico=mysqli_query($conexion, $ConsultaCorreo_Electronico);
+$ConsultaCorreo_Electronico=mysqli_fetch_array($ConsultaCorreo_Electronico);// el fetch es porque nos devolvera un array o un null
+
 //Aqui se anade los datos a la base de datos
+if(!$ConsultaCorreo_Electronico){
+
 $sql = "INSERT INTO Usuario VALUES ('$Nombre','$Apellido','$Municipio','$Colonia','$Ciudad','$Codigo_Postal','$Edad','$Contrasena','$Telefono','$Direccion','$Correo_Electronico')";
 
 //Aqui se verifica si se guardaron los datos
@@ -27,9 +34,15 @@ if(mysqli_query($conexion, $sql)){
 
     echo"Tu cuenta ha sido creada.";
     Echo"<br> <a href= link de la pagina login>Iniciar sesion</a></div>";
+}else{
+    echo"ERROR" . $sql . "</br>" . mysqli_connect_error($conexion);
 }
 
 // Se cierra la conexion
 mysqli_close($conexion);
+}else{
+    echo"El correo ya existe"
+    Echo"<br> <a href= link de la pagina login>Intentalo de nuevo</a></div>";
+}
 
 ?>
